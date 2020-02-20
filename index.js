bd5ca279f46a1211fd1e3dcb23ffd066e5edbb75
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-02-20 15:09:42 +0100

    Update UI :)

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const redis = require("redis");
+const exphbs = require("express-handlebars")
+const path = require("path");
+require("dotenv").config();
 
 const client = redis.createClient({
     'url': process.env.REDIS_URL || "redis://localhost:6379"
 });
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", exphbs({"defaultLayout": "main"}));
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
     client.get("counter", (err, reply) => {
         let obj = reply ? JSON.parse(reply) : {"counter": 0};
         obj.counter++;
         client.set("counter", JSON.stringify(obj), (err) => {
-            res.type("text/plain");
-            res.send("Counter is " + obj.counter).end();
+            res.render("root", obj);
         })
     })
 })
